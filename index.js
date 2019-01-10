373e96917670b400a1f597e95130a2d9edc6f6c1
Zakeer <zakeer@allwyncorp.com>
2019-01-10 07:56:31 +0530

    today changes

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const config = require('./config/database');
+const path = require('path');
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello</h1>');
+mongoose.Promise = global.Promise;
+mongoose.connect(config.uri, (err) => {
+    if (err) {
+        console.log('Could NOT connect to database: ', err);
+    } else {
+        console.log('Connect to database: ' + config.db);
+    }
+});
+
+app.use(express.static(__dirname + '/client/dist/client/'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/dist/client/index.html'));
 });
 
 app.listen(8080, () => {
